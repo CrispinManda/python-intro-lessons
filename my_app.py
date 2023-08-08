6e0992fe2634042865280dfb03a682f0d10e24ab
Crispin Manda <crispinmanda06@gmail.com.com>
2023-08-08 11:19:29 +0300

    first commit

---
+++ my_app.py
@@ -0,0 +1,46 @@
+
+# name = input ('What is your name?')
+
+# print('hello '+ name)
+# -----------------------------------------
+# -----------------------------------------
+
+# variables
+# name = 'Jamila'
+# Age = 18
+# pi = 3.14
+# cars = ['bmw', 'mercedes', 'range rover'] 
+
+# print(name)
+# print(Age)
+# print (pi, cars[0])
+# print(cars)
+
+# -----------------------------------------
+# -----------------------------------------
+# strings
+# first_name = 'Jamila'
+# surname = 'Smith'
+# full_name = first_name + ' ' +  surname
+# print(full_name)
+# print(len(first_name))
+# print(len(surname))
+
+# -----------------------------------------
+# number
+# -----------------------------------------
+# number
+# addition =  10 + 5
+# subtraction = 10 - 5
+# multiplication = 10 * 5
+# division = 10 / 5
+# print(subtraction )
+# print(addition )
+# print(multiplication )
+# print(division )
+# mod = 10 % 3
+# print (mod)
+
+# -----------------------------------------
+# boolean
+# -----------------------------------------
